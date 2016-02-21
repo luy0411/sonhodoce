9943a9bfdcf1526de2abdfb49717ef37801205b6
Luy Santos <luy0411@gmail.com>
2016-02-21 20:29:23 -0300

    Last version

--- js/sd.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {  
-   $("#carousel").swiperight(function() {  
-      $("#carousel").carousel('prev');  
-    });  
-   $("#carousel").swipeleft(function() {  
-      $("#carousel").carousel('next');  
-   });  
-});  
